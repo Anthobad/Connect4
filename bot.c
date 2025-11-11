b04619bc7b7b786190d65c09e3fd5f8603485b24
Anthobad <anthonybadawi100@gmail.com>
2025-11-11 19:13:30 +0200

    Time complexity estimation the medium bot algo. with specific ROWS and COLS

--- bot.c
+++ bot.c
@@ -15,13 +15,13 @@ int bot_choose_move(const Board* g) {
 	return valid[i];
 }
 
-int bot_choose_move_medium(const Board* g) {
+int bot_choose_move_medium(const Board* g) { //Time complexity of this algorith is O(ROWS * COLS) + O(COLS) worst case, O(ROWS * COLS) average case
 	int blocking_cols[COLS];
 	int num_blocking = 0;
 	char human = 'A';
 
-	for (int c = 0; c < COLS; c++) {
-		int r = game_can_drop(g, c);
+	for (int c = 0; c < COLS; c++) { //O(COLS)
+		int r = game_can_drop(g, c); //O(ROWS)
 		if (r == -1) {
 			continue;
 		}
@@ -37,5 +37,5 @@ int bot_choose_move_medium(const Board* g) {
 		return blocking_cols[idx];
 	}
 
-	return bot_choose_move(g);
+	return bot_choose_move(g); //O(COLS)
 }
